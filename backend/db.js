b2fe60620b9d5b124ff18b67c11a80c276339d60
Aritra Ghose <ar1tragh05e9@gmail.com>
2024-12-14 16:38:00 +0600

    database schemas added

---
+++ backend/db.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+function connectDatabase() {
+  try {
+    mongoose.connect(connectionString);
+    console.log("Sucessfully connected to the Database.")
+  } catch(error) {
+    console.log("Error connecting to the database.")
+  }
+}
+
+connectDatabase();
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 30
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  }
+})
+
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+})
+
+
+const User = mongoose.model("users", userSchema);
+const Account = mongoose.model("accounts", accountSchema);
+
+module.exports = {
+  User,
+  Account
+};
